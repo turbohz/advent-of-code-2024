d942af36e7a64296dddb3593c4b89de4b033a41a
TurboHz <turbohz@gmail.com>
2024-12-07 18:57:21 +0100

    Adds Input type, implementing a method to get parsed items for each line

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,6 +1,19 @@
 mod day01;
 
 use super::*;
+use peg::{error::ParseError, str::LineCol};
+
+struct Input<'a>(&'a str);
+impl<'a> Input<'a> {
+	/// Given a parser function, returns an iterator of parsed items of type T.
+	/// Panics if parsing fails.
+	fn iter<T>(&self, parse:fn(&'a str) -> Result<T,ParseError<LineCol>>)-> impl Iterator<Item=T> + use<'a,T> {
+		// clean up input comming from inline examples
+		let raw_lines = self.0.lines().map(str::trim_start).filter(|l| !l.is_empty());
+		// return a lazy parsing iterator
+		raw_lines.map(move |l| parse(l).expect("Parser should not fail"))
+	}
+}
 
 fn try_submit(day:usize, solver:Solver)->Result<(),AppError> {
 	use aoc_driver::{calculate_and_post, Part::*};
